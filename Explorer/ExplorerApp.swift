600971fe33c5235dd9887b7afcb4fb05eb69e0e7
Jared Grimes <jaredlgrimes@hotmail.com>
2023-05-25 19:54:39 -0400

    Initial Commit

---
+++ Explorer/ExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExplorerApp.swift
+//  Explorer
+//
+//  Created by Jared Grimes on 5/25/23.
+//
+
+import SwiftUI
+
+@main
+struct ExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
